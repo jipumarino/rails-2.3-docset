920b7b3b524fbe3e09336ba2f5a17f35a638eaac
Juan Ignacio Pumarino <jipumarino@gmail.com>
2012-05-18 10:12:25 -0400

    Mejora script de actualización de feed

--- update_feed.sh
+++ update_feed.sh
@@ -1,3 +1,9 @@
 version=`date +'%Y%m%d%H%M'`
 tar --exclude='.DS_Store' -czf feed/Ruby\ on\ Rails\ 2.3.tgz Ruby\ on\ Rails\ 2.3.docset
-sed -e "s/VERSION/$version/" feed/Ruby\ on\ Rails\ 2.3.xml.template > feed/Ruby\ on\ Rails\ 2.3.xml
+cat > feed/Ruby\ on\ Rails\ 2.3.xml <<FEED_XML
+<entry>
+    <version>$version</version>
+    <url>https://github.com/jipumarino/rails-2.3-docset/blob/master/feed/Ruby%20on%20Rails%202.3.tgz</url>
+</entry>
+FEED_XML
+
